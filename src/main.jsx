68738ed8b96ae3c2f0dcbc46d1e286820fc6893f
David Barral <contact@davidbarral.com>
2016-04-06 13:07:42 +0200

    Redux boilerplate

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,31 @@ import "bootstrap/css/bootstrap";
 import "app/custom-styles";
 import React from "react";
 import { render } from "react-dom";
-
+import thunk from "redux-thunk";
+import { createStore, compose, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import appReducer from "app/reducers";
 import CommentedPicture from "app/components";
 
+
+const store = createStore(
+  appReducer,
+  compose(
+    applyMiddleware(thunk),
+    window.devToolsExtension ? window.devToolsExtension() : f => f
+  )
+);
+
+if (module.hot) {
+  module.hot.accept("./reducers", () => {
+    const nextReducer = require("./reducers").default;
+    store.replaceReducer(nextReducer);
+  });
+}
+
 render(
-  <CommentedPicture />,
+  <Provider store={store}>
+    <CommentedPicture />
+  </Provider>,
   document.getElementById("app")
 );
