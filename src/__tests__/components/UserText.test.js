25562b5a18dd475cdcb8e1cba601808f1d0282d8
Diana Yáñez <diana.yanez@trabe.io>
2018-07-03 09:38:48 +0200

    Add test for user text deletion

--- src/__tests__/components/UserText.test.js
+++ src/__tests__/components/UserText.test.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import toJson from 'enzyme-to-json';
-import configureStore from 'redux-mock-store'; // Smart components
 
 import UserText from '../../components/UserText';
 
-describe('<UserText key={1} text="Awesome comment" author="Finn" id={1} onDeleteClick={console::log} />', () => {
+describe('<UserText ... />', () => {
   describe('render()', () => {
     test('renders the user text', () => {
+      const onDeleteClick = jest.fn();
       const wrapper = shallow(
-        <UserText key={1} text="Awesome comment" author="Finn" id={1} onDeleteClick={console.log} />
+        <UserText key={1} text="Awesome comment" author="Finn" id={1} onDeleteClick={onDeleteClick} />
       );
       expect(wrapper).toMatchSnapshot();
+    });
+  });
+
+  describe('successfully calls the onDeleteClick handler', () => {
+    test('onDeleteClick works', () => {
+        const onDeleteClick = jest.fn();
+        const component = shallow(
+            <UserText key={1} text="Awesome comment" author="Finn" id={1} onDeleteClick={onDeleteClick} />
+        );
+        component.find('img').simulate('click', { preventDefault: ()=> undefined })
+        expect(onDeleteClick).toHaveBeenCalledTimes(1);
     });
   });
 });
